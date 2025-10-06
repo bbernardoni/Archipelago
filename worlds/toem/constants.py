7379fd3d493edcf4390150c644b183f0a1b7f761
Bennett Bernardoni <bennett.bernardoni@gmail.com>
2025-10-06 19:26:49 -0400

    First proper release test. Mostly went fine

--- worlds/toem/constants.py
+++ worlds/toem/constants.py
@@ -1,14 +1,3 @@
 from typing import Final, final
 
 GAME_NAME: Final[str] = "TOEM: A Photo Adventure"
-
-
-@final
-class Area:
-    HOMELANDA = "Homelanda"
-    OAKLAVILLE = "Oaklaville"
-    STANHAMN = "Stanhamn"
-    LOGCITY = "Logcity"
-    KIIRUBERG = "Kiiruberg"
-    MOUNTAIN_TOP = "Mountain Top"
-    BASTO = "Basto"
